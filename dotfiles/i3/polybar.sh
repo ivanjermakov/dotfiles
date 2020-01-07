768c9e91c9c8593cfd3f1dbd422944380e684ab4
ivanjermakov <vania6600@gmail.com>
2020-01-07 22:28:51 +0300

    update

--- dotfiles/i3/polybar.sh
+++ dotfiles/i3/polybar.sh
@@ -4,10 +4,9 @@
 killall -q polybar
 
 # Wait until the processes have been shut down
-# while pgrep -x polybar >/dev/null; do sleep 1; done
+while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch polybar based on screen
 xrandr | grep 'HDMI-1 connected' && polybar main &
 xrandr | grep 'HDMI-1 disconnected' && polybar main4k &
-#polybar main &
 
