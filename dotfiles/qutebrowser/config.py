539f060203c52010f71b5ad57e3e77008191c4dd
ivanjermakov <ivanjermakov1@gmail.com>
2022-01-26 20:37:10 +0100

    update

--- dotfiles/qutebrowser/config.py
+++ dotfiles/qutebrowser/config.py
@@ -3,29 +3,34 @@ config.load_autoconfig(False)
 dark = '#141414'
 gray = '#444444'
 
-#use ijkl instead of hjkl
 config.unbind('h')
 config.unbind('H')
-config.bind('i', ':scroll up')
-config.bind('I', ':scroll-to-perc 0')
 config.unbind('j')
 config.unbind('J')
-config.bind('j', ':scroll-page -.5 -.5')
-config.bind('J', ':tab-prev')
-config.bind('<Alt-j>', ':back')
-config.bind('<Alt-Left>', ':back')
 config.unbind('k')
 config.unbind('K')
-config.bind('k', ':scroll down')
-config.bind('K', ':scroll-to-perc')
-config.bind('<Alt-k>', ':forward')
-config.bind('<Alt-Right>', ':forward')
 config.unbind('l')
 config.unbind('L')
-config.bind('l', ':scroll-page .5 .5')
-config.bind('L', ':tab-next')
-config.bind (',m', ':spawn mpv {url}')
-config.bind (',M', ':hint links spawn mpv {hint-url}')
+
+c.bindings.commands = {
+    'normal': {
+        'i': ':scroll up',
+        'j': ':scroll-page -.5 -.5',
+        'k': ':scroll down',
+        'l': ':scroll-page .5 .5',
+        'I': ':scroll-to-perc 0',
+        'K': ':scroll-to-perc',
+        'J': ':tab-prev',
+        'L': ':tab-next',
+        '<Alt-j>': ':back',
+        '<Alt-Left>': ':back',
+        '<Alt-k>': ':forward',
+        '<Alt-Right>': ':forward',
+        '!': 'set-cmd-text :open -t -r !',
+        ',m': ':spawn mpv {url}',
+        ',M': ':hint links spawn mpv {hint-url}'
+    }
+}
 
 #key bindings
 config.bind('0', ':zoom')
@@ -35,18 +40,28 @@ config.bind('F', ':hint all tab')
 config.bind('<Ctrl+l>', 'set-cmd-text -s :open')
 
 #web pages
-c.colors.webpage.darkmode.enabled = True
+#c.colors.webpage.darkmode.enabled = True
 c.colors.webpage.preferred_color_scheme = 'dark'
 c.colors.webpage.bg = dark
 c.zoom.default = 120
+
+#css
+#c.content.user_stylesheets = ['~/.config/qutebrowser/css/dark/dark-all-sites.css'] 
 
 #ui
 c.fonts.default_size = '16pt'
 c.fonts.default_family = 'Cantarell'
 c.scrolling.smooth = True
-c.url.searchengines = {'DEFAULT': 'https://www.google.com.ar/search?q={}'}
+c.url.searchengines = {
+    'DEFAULT': 'https://www.google.com.ar/search?q={}',
+    '!w': 'https://en.wikipedia.org/wiki/{}',
+    '!s': 'https://stackoverflow.com/search?q={}',
+}
+c.url.start_pages = ['about:blank']
 c.url.default_page = 'about:blank'
 c.content.notifications.enabled = False
+c.tabs.indicator.width = 0
+c.tabs.last_close = 'startpage'
 
 #color theme
 c.colors.tabs.bar.bg = dark
